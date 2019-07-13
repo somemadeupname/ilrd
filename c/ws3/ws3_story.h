3ab2fdb46c7969309ada80b817444c6a5362b12f
ran.shieber <ran.shieber1@gmail.com>
2019-07-13 21:26:43 +0300

    finished code for story without documentation

---
+++ c/ws3/ws3_story.h
@@ -0,0 +1,5 @@
+
+void Init_Generals (int *generals, const int num_generals);
+void Print_Generals (int *generals, const int num_generals);
+void Execute_Generals (int *generals, int starting_general, const int num_generals);
+int Get_Last_General (const int *generals);
