98a98757fb77e2f65fe6a5b4722b92345d916065
ran.shieber <ran.shieber1@gmail.com>
2019-07-15 23:32:16 +0300

    fixed seg fault and destroyer works

--- c/ws3/ws3_environment.c
+++ c/ws3/ws3_environment.c
@@ -1,11 +1,15 @@
 #include "ws3_environment.h"
 
+#define NO_WARNINGS	int no = argc; char* warnings = argv[0]; if ((char)no == *warnings){}
+
 char* StrcpyLower(char *dest, char *src)
 {
-	char* dest_start = dest;
-	/*NULL checks*/
-	assert (dest != NULL);
-	assert (src != NULL);
+	char* dest_start = NULL;
+
+	assert (NULL != dest);
+	assert (NULL != src);
+	
+	dest_start = dest;
 
 	while (*src != '\0')
 	{
@@ -28,7 +32,6 @@ size_t GetEnvSize(char **env)
 		++elements_num;
 		*(++env);
 	}
-	/*printf ("elements_num = %d\n", elements_num);*/
 	return elements_num;
 }
 
@@ -36,7 +39,8 @@ char** InitLowerEnv(char **env)
 {
 	char **lower_env = NULL;
 	size_t num_env_vars = 0;
-	/*TODO assert null on both pointers*/
+
+	assert(NULL != env);
 	
 	num_env_vars = GetEnvSize(env) + 1; /* add one for the null character*/
 	lower_env = (char**) malloc (num_env_vars * sizeof(char*));
@@ -54,6 +58,9 @@ char** InitEnvVars(char **lower_env, char **env)
 	/* TODO assert make sure both aren't null*/
 	size_t env_var_size = 0;
 	
+	assert(NULL != env);
+	assert(NULL != lower_env);
+	
 	while (NULL != *env)
 	{
 		env_var_size = strlen(*env) + 1;/*+ 1 for the '\0' char */
@@ -69,26 +76,28 @@ char** InitEnvVars(char **lower_env, char **env)
 }
 
 void FillLowerEnv (char **lower_env, char **env)
-{
-	char** start = lower_env;
-	
+{	
 	size_t lower_var_len = 0;
 	size_t char_index = 0;
-	/* TODO assert make sure both aren't null*/
+	
+	assert(NULL != env);
+	assert(NULL != lower_env);
+	
 	while (NULL != *env)
 	{
 		*lower_env = StrcpyLower(*lower_env, *env);
 		++lower_env;
 		++env;
 	}
-	
 }
 
 void PrintLowerEnv(char **lower_env)
 {
-	/* TODO assert make sure aren't null*/
+	char **cur_lower_env = NULL;
 	
-	char ** cur_lower_env = (char**) lower_env;
+	assert(NULL != lower_env);
+	
+	cur_lower_env = (char**) lower_env;
 	
 	while (NULL != *cur_lower_env)
 	{
@@ -99,16 +108,17 @@ void PrintLowerEnv(char **lower_env)
 		
 void DestroyLowerEnv(char** lower_env)
 {
-	/*TODO assert NULL*/
-	
-	size_t lower_env_size = GetEnvSize (lower_env) + 1;
-	char* lower_env_cur = *(lower_env + lower_env_size);
-	
-	while ( lower_env_cur != *lower_env )
+	size_t lower_env_size = 0;
+	char* lower_env_cur	= NULL;
+	int i = 1;
+
+	assert(NULL != lower_env);
+	lower_env_size = GetEnvSize(lower_env);
+	for (i = 1; i <= lower_env_size; ++i)
 	{
+		lower_env_cur = *(lower_env + lower_env_size - i);
 		free(lower_env_cur); lower_env_cur = NULL;
-		--lower_env_cur;
-	}	
+	}
 	free(lower_env); lower_env = NULL;
 }
 	
@@ -116,17 +126,17 @@ void DestroyLowerEnv(char** lower_env)
 	
 
 int main(int argc, char *argv[], char **envp)
-{
+{	
 	char **lower_env = NULL;
-	char s[] = "test";	
 	
-	/*TODO isn't NULL and malloc succeded*/
+	NO_WARNINGS
 	
 	lower_env = InitLowerEnv(envp);
 	
 	if ( NULL == InitEnvVars (lower_env, envp) )
 	{
 		printf("failed allocating memory\n");
+		return (-1);
 	}
 	
 	FillLowerEnv(lower_env, envp);
