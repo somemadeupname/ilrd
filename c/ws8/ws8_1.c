642204593658ea8ccda5e52bb9854ab2a14c1826
ran.shieber <ran.shieber1@gmail.com>
2019-07-29 17:14:14 +0300

    changed implementation to have value stored instead of address

--- c/ws8/ws8_1.c
+++ c/ws8/ws8_1.c
@@ -1,20 +1,22 @@
-#include <stdio.h> /* printf */
-#include <stdlib.h> /* malloc realloc free */
+#include <stdio.h> /* printf, sprintf */
+#include <stdlib.h> /* malloc, realloc, free */
 #include <string.h> /* strcat */
 #include <stddef.h> /* size_t */
+#include <assert.h>
 
 #define UNUSED(x) (void)(x)
+#define MAX_DIGITS_SIZE_OF_INT 12
 
 /************* FUNCTIONS DECLARATIONS *************/
-void AddInt(void *data, void *input);
+void AddInt(void *data, int input);
 void PrintInt(void *data);
 void FreeInt(void *data);
 
-void AddFloat(void *data, void *input);
+void AddFloat(void *data, int input);
 void PrintFloat(void *data);
 void FreeFloat(void *data);
 
-void AddString(void *data, void *input);
+void AddString(void *data, int input);
 void PrintString(void *data);
 void FreeString(void *data);
 
@@ -22,7 +24,7 @@ void FreeString(void *data);
 typedef struct 
 {
 	void *data;
-	void (*add)(void *data, void *input);
+	void (*add)(void *data, int input);
 	void (*print)(void *data);
 	void (*free)(void *data);
 } struct_element;
@@ -30,98 +32,90 @@ typedef struct
 
 int main()
 {
-	int num = 3;
 	int input = 6;
-	char *str_input = "55";
-	
+	int num = 13;
 	float f_num = 3.7f;
+	size_t index = 0;	
 	
 	struct_element arr[] =
 	{
-		{ (void *) NULL , &AddInt, &PrintInt, &FreeInt },
-		{ (void *) NULL , &AddFloat, &PrintFloat, &FreeFloat },
-		{ (void *) NULL , &AddString, &PrintString, &FreeString }		
+		{(void *) NULL, &AddInt, &PrintInt, &FreeInt},
+		{(void *) NULL, &AddFloat, &PrintFloat, &FreeFloat},
+		{(void *) NULL, &AddString, &PrintString, &FreeString}		
 	};
 	
+	/* intialization of data members in each union-esqe object */
 	
-	/* will later be a loop */
-	
-	arr[0].data = &num;
-	arr[0].add(arr[0].data, (void *)&input);
-	arr[0].print(arr[0].data);
-	arr[0].free(arr[0].data);
+	arr[0].data = *((void **) &num);
+	arr[1].data = *((void **) &f_num);
+	arr[2].data = (char *) malloc ((5 + MAX_DIGITS_SIZE_OF_INT * sizeof(char)));
+	/* check if malloc succeeds*/
+	strcpy(arr[2].data, "hello");
 
-	arr[1].data = &f_num;
-	arr[1].add(arr[1].data, (void *)&input);
-	arr[1].print(arr[1].data);
-	arr[1].free(arr[1].data);
-	
-	arr[2].data = (char *) malloc (5 * sizeof(char));
-	/* check if malloc succeeds */
-	strcpy(arr[2].data,"yolo");
-	arr[2].add(arr[2].data, (void *) str_input);
-	arr[2].print(arr[2].data);
-	arr[2].free(arr[2].data);	
+
+	for (index = 0; index < (sizeof(arr)/sizeof(arr[0])); ++index)
+	{
+		/* TODO remove printf debug */
+		printf("working on arr[%ld]:\n", index);
+		arr[index].add(&arr[index].data, input);
+		arr[index].print(arr[index].data);
+		arr[index].free(arr[index].data);
+	}
 	
 	return 0;
 }
 /************ Int Functions *******************/
-void AddInt(void *data, void *input)
+void AddInt(void *data, int input)
 {
-	/* NULL asserts */
-	*(int *) data += *(int *) input;
+	assert(NULL != data);
+	*(int *)data += input;
 	
 }
 void PrintInt(void *data)
 {
-	/* NULL asserts */
-	printf("%d\n", *(int *)data);
+	assert(NULL != data);
+	printf("%d\n", *(int *)&data);
 }
 void FreeInt(void *data)
 {
-	UNUSED(data);
+	assert(NULL != data);	
+	UNUSED(&data);
 	/* do nothing */
 }
 
 /************ Float Functions *******************/
-void AddFloat(void *data, void *input)
+void AddFloat(void *data, int input)
 {
-	/* NULL asserts */
-	*(float *) data += *(int *) input;
+	assert(NULL != data);
+	*(float *) data += input;
 }
 void PrintFloat(void *data)
 {
-	/* NULL asserts */
-	printf("%f\n", *(float *)data);
+	assert(NULL != data);
+	printf("%f\n", *(float *)&data);
 }
 void FreeFloat(void *data)
-{
-	UNUSED(data);
+{	
+	UNUSED(&data);
 	/* do nothing */
 }
 
 /************ String Functions *******************/
-void AddString(void *data, void *input)
+void AddString(void *data, int input)
 {
-	/* NULL asserts */
-	size_t added_length = strlen((char *) input);
-	size_t original_length = strlen((char *) data);	
-	
-	printf("input=%s\n", (char *) input);
-	printf("added_length + original_length + 1=%ld\n", added_length + original_length + 1);
-	data = (char *) data;
-	data = (char *) realloc (data, added_length + original_length + 1);
-	strcat (data, input);
-	/* check that strcat worked*/
+	char digits[12]; /* max size of int digits */;
+	assert(NULL != data);
+	sprintf(digits, "%d", input); /* save input as string into digits */
+	strcat (*(char **)data, digits);	
 }
 void PrintString(void *data)
 {
-	/* NULL asserts */
+	assert(NULL != data);
 	printf("%s\n", (char *)data);
 }
 void FreeString(void *data)
 {
-	/* null asserts */
+	assert(NULL != data);
 	free(data);
 	data = NULL;
 }
