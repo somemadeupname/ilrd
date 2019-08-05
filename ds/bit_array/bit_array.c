fc9d28cf658755d9f3bec5540ce0948265672821
ran.shieber <ran.shieber1@gmail.com>
2019-08-05 20:20:00 +0300

    finishes 15/17 functions

--- ds/bit_array/bit_array.c
+++ ds/bit_array/bit_array.c
@@ -2,6 +2,9 @@
 #include <string.h> /* strlen */
 #include "bit_array.h"
 
+#define NUM_MAX_BITS 64
+
+
 int BitArrIsBitSet(bitarray_t bit_array, size_t bit_index)
 {
 	return ((bit_array & ( 1LU << bit_index)) > 0);
@@ -14,20 +17,23 @@ size_t BitArrCountOnBits(bitarray_t bit_array)
 	
 	while (bit_array > 0)
 	{
-		if (bit_array & mask) /* check if the rightmost bit is on in bit_array */
+		/* check if the rightmost bit is on in bit_array */
+		if (bit_array & mask) 
 		{
 			++num_of_on_bits;
 		}
-		bit_array >>= 1; /* decrease bit_array by dividing it by two */
+		/* decrease bit_array by dividing it by two */		
+		bit_array >>= 1;
 	}
 	return num_of_on_bits;
 }
 
 size_t BitArrCountOffBits(bitarray_t bit_array)
 {
-	return (64LU - BitArrCountOnBits(bit_array));
+	return (NUM_MAX_BITS - BitArrCountOnBits(bit_array));
 }
-
+/* helper function for BitArrBitToStr which reverses
+   the string bit representation 					 */
 static void ReverseString(char *str)
 {
 	char *reverse = str;
@@ -54,9 +60,9 @@ char *BitArrBitToStr(bitarray_t bit_array, char *str)
 	size_t index = 0;
 	assert(NULL != str);
 	
-	for (index = 0; index < 64; ++index)
+	for (index = 0; index < NUM_MAX_BITS; ++index)
 	{
-		if (bit_array & mask) /* check if the rightmost bit is on in bit_array */
+		if (bit_array & mask)/* check if the rightmost bit is on in bit_array */
 		{
 			*str = (char)(1 + '0');
 		}
@@ -76,36 +82,53 @@ char *BitArrBitToStr(bitarray_t bit_array, char *str)
 
 bitarray_t BitArrSetBitOn(bitarray_t bit_array, size_t bit_index)
 {
+	/* bring the set bit to the bit_index
+	location and switch it on in bit_array */
 	unsigned long mask = 1LU;	
 	return (bit_array | (mask << bit_index));
 }
 
-bitarray_t BitArrSetBitOff(bitarray_t bit_array, size_t bit_index);
+bitarray_t BitArrSetBitOff(bitarray_t bit_array, size_t bit_index)
 {
+	unsigned long full_mask = 0xFFFFFFFFFFFFFFFFLU;
 	unsigned long mask = 1LU;	
-	return (bit_array & (mask << bit_index));
+	full_mask = full_mask & ~(mask << bit_index);
+	return (bit_array & full_mask);
 }
-/* TODO test this*/
+
 bitarray_t BitArrSetBit(bitarray_t bit_array, size_t bit_index, int value)
 {
-	bitarray_t ba = bit_array;
+	unsigned long mask = 1LU;		
 	if (1 == value)
 	{
-		ba = (bit_array | (1 << bit_index));
+		bit_array = (bit_array | (mask << bit_index));
 	}
-	else
+	else /* if (0 == value) */
 	{
-		ba = (bit_array | (0 << bit_index));	
+		bit_array = (bit_array & ~(mask << bit_index));	
 	}		
-	return ba;
+	return bit_array;
 }
 
 bitarray_t BitArrFlipBit(bitarray_t bit_array, size_t bit_index)
 {
-	unsigned long mask = 1LU;	
-	return (bit_array ~(mask << bit_index));
+	unsigned long mask = 1LU;
+	unsigned long bit_value = bit_array & (mask << bit_index);
+	unsigned long full_mask = 0xFFFFFFFFFFFFFFFFLU;
+	full_mask = full_mask & ~(mask << bit_index);	
+	
+	if (0 != bit_value) /* if it's on - switch it off */
+	{
+		bit_array = bit_array & full_mask;
+	}
+	else /* 0 == bit_value */
+	{
+		bit_array = bit_array | (mask << bit_index);
+	}
+
+	return bit_array;
 }
-/*TODO v not sure about this*/
+
 bitarray_t BitArrFlipAllBits(bitarray_t bit_array)
 {
 	return ~bit_array;
@@ -113,19 +136,123 @@ bitarray_t BitArrFlipAllBits(bitarray_t bit_array)
 
 bitarray_t BitArrSetAllBitsOn(bitarray_t bit_array)
 {
-	return 0xFFFFFFFFFFFFFFFFLU;
+	return bit_array = (bitarray_t) 0xFFFFFFFFFFFFFFFFLU;
 }
 
 bitarray_t BitArrSetAllBitsOff(bitarray_t bit_array)
 {
-	return (bitarray_t) 0;
+	return bit_array = (bitarray_t) 0LU;
+}
+
+bitarray_t BitArrSwapBits(bitarray_t bit_array,
+						  size_t bit_index1,
+						  size_t bit_index2)
+{
+	unsigned long mask = 1LU;
+	unsigned long mask2 = 0xFFFFFFFFFFFFFFFFLU;
+	
+	/* get the bits' values */
+	unsigned long bit1 = ((bit_array>>bit_index1) & mask);
+	unsigned long bit2 = ((bit_array>>bit_index2) & mask);
+	
+	/* switch off bit indices in bit_array */
+	mask2 = (mask2 &~(mask << bit_index1));
+	mask2 = (mask2 &~(mask << bit_index2));	
+	
+	bit_array = bit_array & mask2;
+	
+	/* move the bits to the relevant index and set their values to bit_array */
+	bit_array = (bit_array | bit1 << bit_index2);
+	bit_array = (bit_array | bit2 << bit_index1);
+	
+	return bit_array;
+}
+/*
+ * Function rotates the bit_array one step to the right.
+ * Param bit_array
+ * Returns: The rotated bit array
+ */
+static bitarray_t BitArrRotateOneBitsRight(bitarray_t bit_array)
+{
+	#define RIGHTMOST_BIT_INDEX 0
+	unsigned long rightmost_bit = BitArrIsBitSet(bit_array, RIGHTMOST_BIT_INDEX);
+	unsigned long mask = 0x8000000000000000LU;/* only the rightmost bit is on */
+	bit_array >>= 1LU;
+	if (0 != rightmost_bit)
+	{
+		bit_array = (bit_array | mask);
+	}
+	return bit_array;	
 }
 
 bitarray_t BitArrRotateBitsRight(bitarray_t bit_array, size_t rotate_by)
 {
-	if (64 < rotate_by)
+	size_t index = 0;
+	
+	/* no need to rotate more than the bit_array's size */
+	if (NUM_MAX_BITS < rotate_by)
 	{
-		rotate_by %= 64;
+		rotate_by %= NUM_MAX_BITS;
 	}
 	
+	for (index = 0; index < rotate_by; ++index)
+	{
+		bit_array = BitArrRotateOneBitsRight(bit_array);
+	}
+	
+	return bit_array;
+}
+
+/*
+ * Function rotates the bit_array one step to the left.
+ * Param bit_array
+ * Returns: The rotated bit array
+ */
+static bitarray_t BitArrRotateOneBitsLeft(bitarray_t bit_array)
+{
+	#define LEFTMOST_BIT_INDEX (NUM_MAX_BITS - 1)
+	unsigned long leftmost_bit = BitArrIsBitSet(bit_array, LEFTMOST_BIT_INDEX);
+	unsigned long mask = 1LU;
+	bit_array <<= 1;
+	if (0 != leftmost_bit)
+	{
+		bit_array = bit_array | mask;
+	}
+	return bit_array;
+}
+
+bitarray_t BitArrRotateBitsLeft(bitarray_t bit_array, size_t rotate_by)
+{
+	size_t index = 0;
+	
+	/* no need to rotate more than the bit_array's size */
+	if (NUM_MAX_BITS < rotate_by)
+	{
+		rotate_by %= NUM_MAX_BITS;
+	}
+	
+	for (index = 0; index < rotate_by; ++index)
+	{
+		bit_array = BitArrRotateOneBitsLeft(bit_array);
+	}
+	
+	return bit_array;
+}
+
+bitarray_t BitArrBitMirror(bitarray_t bit_array)
+{
+	/* initializations  */
+	bitarray_t mirrored = 0LU;
+	unsigned long mask = 1LU;
+	size_t bit_index = 0;
+	
+	/* iterate over bit_array bit-by-bit and switch on
+	   		the opposite bit index in mirrored 		   */
+	for (bit_index = 0; bit_index < NUM_MAX_BITS; ++bit_index)
+	{
+		mirrored <<= 1;
+		mirrored |= (bit_array & mask);
+		bit_array >>= 1;
+	}
+	return mirrored;
 }
--- ds/bit_array/bit_array_test.c
+++ ds/bit_array/bit_array_test.c
@@ -2,28 +2,64 @@
 #include "bit_array.h"
 
 /* FORWARD DECLARATIONS */
+/* 1-3 */
 void TestBitArrIsBitSet();
 void TestBitArrCountOnBits();
 void TestBitArrCountOffBits();
+
+/* 4-9 */
 void TestBitArrBitToStr();
 void TestBitArrSetBitOn();
+void TestBitArrSetBitOff();
+void TestBitArrSetBit();
+void TestBitArrFlipBit();
+void TestBitArrFlipAllBits();
+/* 10-11 */
+void TestBitArrSetAllBitsOn();
+void TestBitArrSetAllBitsOff();
+
+/* 12-15 */
+void TestBitArrSwapBits();
+void TestBitArrRotateBitsRight();
+void TestBitArrRotateBitsLeft();
 
 /* main */
 
 int main()
 {
-	
+	/* 1-3 */
 	TestBitArrIsBitSet();
 
 	TestBitArrCountOnBits();
 	
 	TestBitArrCountOffBits();
 
+	/* 4-9 */
 	/*
 	TestBitArrBitToStr();
 	*/
 	
 	TestBitArrSetBitOn();
+	
+	TestBitArrSetBitOff();
+	
+	TestBitArrSetBit();
+	
+	TestBitArrFlipBit();
+	
+	TestBitArrFlipAllBits();
+
+	/* 10-11 */	
+	TestBitArrSetAllBitsOn();
+
+	TestBitArrSetAllBitsOff();
+
+	/* 12-15 */
+	TestBitArrSwapBits();
+	
+	TestBitArrRotateBitsRight();
+	
+	TestBitArrRotateBitsLeft();
 	
 	return 0;
 }
@@ -169,4 +205,259 @@ void TestBitArrSetBitOn()
 		printf("BitArrSetBitOn failed 4.\n");
 		printf("BitArrSetBitOn(0xFFFFFFFFFFFFFFFLU) = %lu.\n", BitArrSetBitOn(ba,56));		
 	}
+}
+
+void TestBitArrSetBitOff()
+{	
+	bitarray_t ba = 5;
+	if (1 != BitArrSetBitOff(ba,2))
+	{
+		printf("BitArrSetBitOff failed 1.\n");
+		printf("BitArrSetBitOff(5,3) = %lu.\n", BitArrSetBitOff(ba,2));								
+	}
+	
+	ba = 10;
+	if (2 != BitArrSetBitOff(ba,3))
+	{
+		printf("BitArrSetBitOff failed 2.\n");
+		printf("BitArrSetBitOff(10,3) = %lu.\n", BitArrSetBitOff(ba,3));	
+	}		
+	
+	ba = 4294967296; /* 2^32 */
+	
+	if (0 != BitArrSetBitOff(ba,32))
+	{
+		printf("BitArrSetBitOff failed 3.\n");
+		printf("BitArrSetBitOff(2^32) = %lu.\n", BitArrSetBitOff(ba,32));				
+	}
+	
+	ba = 0xFFFFFFFFFFFFFFFFLU; 
+	if (0xFEFFFFFFFFFFFFFFLU != BitArrSetBitOff(ba,56))
+	{
+		printf("BitArrSetBitOff failed 4.\n");
+		printf("BitArrSetBitOff(0xFFFFFFFFFFFFFFFLU) = %lu.\n", BitArrSetBitOff(ba,56));		
+	}
+}
+
+void TestBitArrSetBit()
+{	
+	bitarray_t ba = 0;
+	if (8 != BitArrSetBit(ba,3,1))
+	{
+		printf("BitArrSetBit failed 1.\n");
+		printf("BitArrSetBit(0,3,1) = %lu.\n", BitArrSetBit(ba,3,1));								
+	}
+	
+	ba = 10;
+	if (8 != BitArrSetBit(ba,1,0))
+	{
+		printf("BitArrSetBit failed 2.\n");
+		printf("BitArrSetBit(10,1,0) = %lu.\n", BitArrSetBit(ba,1,0));						
+	}		
+	
+	ba = 4294967296; /* 2^32 */
+	
+	if (0 != BitArrSetBit(ba,32,0))
+	{
+		printf("BitArrSetBit failed 3.\n");
+		printf("BitArrSetBit(2^32,32,0) = %lu.\n", BitArrSetBit(ba,32,0));				
+	}
+	
+	ba = 0xFFFFFFFFFFFFFFFFLU; 
+	if (0xFEFFFFFFFFFFFFFFLU != BitArrSetBit(ba,56,0))
+	{
+		printf("BitArrSetBit failed 4.\n");
+		printf("BitArrSetBit(0xFFFFFFFFFFFFFFFLU) = %lu.\n", BitArrSetBit(ba,56,0));		
+	}
+}
+
+void TestBitArrFlipBit()
+{	
+	bitarray_t ba = 0;
+	if (32 != BitArrFlipBit(ba,5))
+	{
+		printf("BitArrFlipBit failed 1.\n");
+		printf("BitArrFlipBit(0,5) = %lu.\n", BitArrFlipBit(ba,5));								
+	}
+	
+	ba = 10;
+	if (8 != BitArrFlipBit(ba,1))
+	{
+		printf("BitArrFlipBit failed 2.\n");
+		printf("BitArrFlipBit(10,1) = %lu.\n", BitArrFlipBit(ba,1));						
+	}		
+	
+	ba = 4294967296; /* 2^32 */
+	
+	if (0 != BitArrFlipBit(ba,32))
+	{
+		printf("BitArrFlipBit failed 3.\n");
+		printf("BitArrFlipBit(2^32,32) = %lu.\n", BitArrFlipBit(ba,32));				
+	}
+	
+	ba = 0xFEFFFFFFFFFFFFFFLU; 
+	if (0xFFFFFFFFFFFFFFFFLU != BitArrFlipBit(ba,56))
+	{
+		printf("BitArrFlipBit failed 4.\n");
+		printf("BitArrFlipBit(0xFFFFFFFFFFFFFFFLU) = %lu.\n", BitArrFlipBit(ba,56));		
+	}
+}
+
+void TestBitArrFlipAllBits()
+{	
+	bitarray_t ba = 0;
+	if (0xFFFFFFFFFFFFFFFFLU != BitArrFlipAllBits(ba))
+	{
+		printf("BitArrFlipAllBits failed 1.\n");
+		printf("BitArrFlipAllBits(0) = %lu.\n", BitArrFlipAllBits(ba));								
+	}
+	
+	ba = 0xFFFFFFFFFFFFFFFELU;
+	if (1 != BitArrFlipAllBits(ba))
+	{
+		printf("BitArrFlipAllBits failed 2.\n");
+		printf("BitArrFlipAllBits(10) = %lu.\n", BitArrFlipAllBits(ba));						
+	}		
+	
+	ba = 4294967296; /* 2^32 */
+	
+	if (0xFFFFFFFFFFFFFFFFLU - 4294967296 != BitArrFlipAllBits(ba))
+	{
+		printf("BitArrFlipAllBits failed 3.\n");
+		printf("BitArrFlipAllBits(2^32) = %lu.\n", BitArrFlipAllBits(ba));				
+	}
+	
+	ba = 1; 
+	if (0xFFFFFFFFFFFFFFFELU != BitArrFlipAllBits(ba))
+	{
+		printf("BitArrFlipAllBits failed 4.\n");
+		printf("BitArrFlipAllBits(1) = %lu.\n", BitArrFlipAllBits(ba));		
+	}
+}
+
+void TestBitArrSetAllBitsOn()
+{
+	bitarray_t ba = 0LU;
+	if (0xFFFFFFFFFFFFFFFFLU != BitArrSetAllBitsOn(ba))
+	{
+		printf("BitArrSetAllBitsOn failed 1.\n");
+		printf("BitArrSetAllBitsOn(0) = %lu.\n", BitArrSetAllBitsOn(ba));								
+	}
+	ba = 0xFFFFFFFFFFFFFFFFLU;
+	if (0xFFFFFFFFFFFFFFFFLU != BitArrSetAllBitsOn(ba))
+	{
+		printf("BitArrSetAllBitsOn failed 1.\n");
+		printf("BitArrSetAllBitsOn(0) = %lu.\n", BitArrSetAllBitsOn(ba));								
+	}	
+}
+
+void TestBitArrSetAllBitsOff()
+{
+	bitarray_t ba = 0LU;
+	if (0LU != BitArrSetAllBitsOff(ba))
+	{
+		printf("BitArrSetAllBitsOff failed 1.\n");
+		printf("BitArrSetAllBitsOff(0) = %lu.\n", BitArrSetAllBitsOff(ba));								
+	}
+	ba = 0xFFFFFFFFFFFFFFFFLU;
+	if (0LU != BitArrSetAllBitsOff(ba))
+	{
+		printf("BitArrSetAllBitsOff failed 1.\n");
+		printf("BitArrSetAllBitsOff(0) = %lu.\n", BitArrSetAllBitsOff(ba));								
+	}	
+}
+
+void TestBitArrSwapBits()
+{
+	bitarray_t ba = 0;
+	if (0 != BitArrSwapBits(ba,2,3))
+	{
+		printf("BitArrSwapBits failed 1.\n");
+		printf("BitArrSwapBits(0,2,3) = %lu.\n", BitArrSwapBits(ba,2,3));								
+	}
+	
+	ba = 0xFFFFFFFFFFFFFFFELU;
+	if (0x7FFFFFFFFFFFFFFFLU != BitArrSwapBits(ba,63,0))
+	{
+		printf("BitArrSwapBits failed 2.\n");
+		printf("BitArrSwapBits(0xFFFFFFFFFFFFFFFELU,63,0) = %lu.\n", BitArrSwapBits(ba,63,0));						
+	}		
+	
+	ba = 4294967296; /* 2^32 */
+	
+	if (2147483648/* 2^31 */ != BitArrSwapBits(ba,31,32))
+	{
+		printf("BitArrSwapBits failed 3.\n");
+		printf("BitArrSwapBits(2^32) = %lu.\n", BitArrSwapBits(ba,31,30));				
+	}
+	
+	ba = 1; 
+	if (0x8000000000000000LU != BitArrSwapBits(ba,0,63))
+	{
+		printf("BitArrSwapBits failed 4.\n");
+		printf("BitArrSwapBits(1,0,63) = %lu.\n", BitArrSwapBits(ba,0,63));		
+	}
+}
+
+void TestBitArrRotateBitsRight()
+{
+	bitarray_t ba = 0;
+	if (0 != BitArrRotateBitsRight(ba,3))
+	{
+		printf("BitArrRotateBitsRight failed 1.\n");
+		printf("BitArrRotateBitsRight(0,3) = %lu.\n", BitArrRotateBitsRight(ba,3));								
+	}
+	
+	ba = 0xFFFFFFFFFFFFFFFELU;
+	if (0xDFFFFFFFFFFFFFFFLU != BitArrRotateBitsRight(ba,3))
+	{
+		printf("BitArrRotateBitsRight failed 2.\n");
+		printf("BitArrRotateBitsRight(0xFFFFFFFFFFFFFFFELU,3) = %lu.\n", BitArrRotateBitsRight(ba,3));						
+	}		
+	
+	ba = 4294967296; /* 2^32 */
+	
+	if (2147483648/* 2^31 */ != BitArrRotateBitsRight(ba,65))
+	{
+		printf("BitArrRotateBitsRight failed 3.\n");
+		printf("BitArrRotateBitsRight(2^32) = %lu.\n", BitArrRotateBitsRight(ba,31));				
+	}
+	
+	ba = 1; 
+	if (0x1000000000000000LU != BitArrRotateBitsRight(ba,4))
+	{
+		printf("BitArrRotateBitsRight failed 4.\n");
+		printf("BitArrRotateBitsRight(1,0) = %lu.\n", BitArrRotateBitsRight(ba,4));		
+	}		
+}
+void TestBitArrRotateBitsLeft()
+{
+	bitarray_t ba = 0;
+	if (0 != BitArrRotateBitsLeft(ba,3))
+	{
+		printf("BitArrRotateBitsLeft failed 1.\n");
+		printf("BitArrRotateBitsLeft(0,3) = %lu.\n", BitArrRotateBitsLeft(ba,3));								
+	}
+	
+	ba = 0xFFFFFFFFFFFFFFFELU;
+	if (0xFFFFFFFFFFFFFFF7LU != BitArrRotateBitsLeft(ba,3))
+	{
+		printf("BitArrRotateBitsLeft failed 2.\n");
+		printf("BitArrRotateBitsLeft(0xFFFFFFFFFFFFFFFELU,3) = %lu.\n", BitArrRotateBitsLeft(ba,3));						
+	}		
+	
+	ba = 4294967296; /* 2^32 */
+	
+	if (8589934592/* 2^33 */ != BitArrRotateBitsLeft(ba,65))
+	{
+		printf("BitArrRotateBitsLeft failed 3.\n");
+		printf("BitArrRotateBitsLeft(2^32) = %lu.\n", BitArrRotateBitsLeft(ba,31));				
+	}
+	
+	ba = 1; 
+	if (0x0000000000000010LU != BitArrRotateBitsLeft(ba,4))
+	{
+		printf("BitArrRotateBitsRight failed 4.\n");
+		printf("BitArrRotateBitsRight(1,0) = %lu.\n", BitArrRotateBitsLeft(ba,4));		
+	}		
 }
