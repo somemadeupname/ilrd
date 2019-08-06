d55eedf81a728be994fcc49a4a08437862b0a721
ran.shieber <ran.shieber1@gmail.com>
2019-08-06 15:59:30 +0300

    finished push

--- ds/stack/stack.c
+++ ds/stack/stack.c
@@ -1,28 +1,31 @@
 #include <stddef.h> /* size_t */
 #include <stdlib.h> /* malloc free */
+#include <string.h> /* memmove */
+#include <assert.h> /* assert */
 #include "stack.h"
 
 #define SIZE_OF_POINTER 8
 #define NUM_OF_ELEMENTS_IN_STRUCT 4
 
-typedef struct stack
+struct stack
 {
 	void *base;
 	void *current;
 	void *end;
 	size_t size_of_element;
-} stack_t;
+};
 
 stack_t *StackCreate(size_t max_num_of_elements, size_t size_of_element)
 {
-	stack_t *stack = (stack *)malloc(NUM_OF_ELEMENTS_IN_STRUCT*SIZE_OF_POINTER);
+	stack_t *stack = (stack_t *)malloc(NUM_OF_ELEMENTS_IN_STRUCT*SIZE_OF_POINTER);
+	void* base = NULL;
 	
 	if (NULL == stack) /* if malloc failed */
 	{
 		return NULL;
 	}
 	
-	void *base = (void *) malloc (size_of_element * max_num_of_elements);
+	base = (void *) malloc (size_of_element * max_num_of_elements);
 	
 	if (NULL == base) /* if malloc failed */
 	{
@@ -30,12 +33,47 @@ stack_t *StackCreate(size_t max_num_of_elements, size_t size_of_element)
 		return NULL;
 	}
 	
-	/* stack initialization */
+	/* stack members initialization */
 	stack->base = base;
 	stack->current = base;
-	stack->end = base + max_num_of_elements;
+	stack->end = (char *)base + max_num_of_elements*size_of_element;
 	stack->size_of_element = size_of_element;
 	
 	return stack;
 }
 
+void StackDestroy(stack_t *stack)
+{
+	free(stack->base); stack->base = NULL;
+	free(stack); stack= NULL;
+}
+
+/* helper function to see if stack is full */
+static int IsStackFull(stack_t *stack)
+{
+	assert(NULL != stack);
+	return (stack->current == stack->end);
+}
+
+int StackPush(stack_t *stack, const void *element)
+{
+	if (IsStackFull(stack))
+	{
+		return STACK_OVERFLOW_ERROR;
+	}
+	
+	memmove(stack->current, element, stack->size_of_element);
+	stack->current = (char *) stack->current + stack->size_of_element;
+	
+	return STACK_SUCCESS;
+}
+
+/*
+ * Removes top element in stack.
+ * Param stack: pointer to a stack
+ * Returns: STACK_SUCCESS if the pop was successfull. Non-zero otherwise.
+ * Errors:  returns STACK_UNDERFLOW_ERROR if stack is empty. Stack remains
+ *		    unchanged.
+ * 			If stack points to an unreadable address, behavior is undefined.
+ */
+int StackPop(stack_t *stack);
--- ds/stack/stack_test.c
+++ ds/stack/stack_test.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include "stack.h"
+
+#define CHECK(X) (printf("%d\n", X))
+
+void TestCreateAndDestroy();
+void TestStackPush();
 
 int main()
 {
+	TestCreateAndDestroy();
+	TestStackPush();	
 	return 0;
 }
+
+void TestCreateAndDestroy()
+{
+	stack_t *stack = StackCreate(4,4);
+	StackDestroy(stack);
+}
+
+void TestStackPush()
+{
+	stack_t *stack = StackCreate(4,4);
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	int e = 5;			
+	CHECK(StackPush(stack, &a));
+	CHECK(StackPush(stack, &b));
+	CHECK(StackPush(stack, &c));
+	CHECK(StackPush(stack, &d));
+	CHECK(StackPush(stack, &e));		
+	
+	StackDestroy(stack);	
+}
+
