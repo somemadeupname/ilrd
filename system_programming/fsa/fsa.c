0f326012147353cece1a805a7799c77da9ba2761
ran.shieber <ran.shieber1@gmail.com>
2019-08-29 19:58:00 +0300

    added suggest and alloc

--- system_programming/fsa/fsa.c
+++ system_programming/fsa/fsa.c
@@ -4,44 +4,24 @@
  *   Reviewer : 		    *
  *	 Status   : Sent	    *
  ****************************/
-
+#include <assert.h>
 #include "fsa.h"
 
 #define WORD_SIZE sizeof(size_t)
+#define FSA_START_OFFSET sizeof(fsa_t)
+
+/* Forward Declarations */
+static size_t AlignBlockSize(size_t block_size);
 
 typedef struct meta
 {
-	size_t next_node;
+	size_t block;
 } meta_t;
 
 struct fsa
 {
-	size_t free_block_index;
-}
-
-/* 
- * helper for init
- * calculate and return aligned block size 
- */
-static size_t AlignBlockSize(size_t block_size)
-{
-	size_t remainder = block_size % WORD_SIZE;
-	size_t = 0;
-	
-	if (block_size < WORD_SIZE)
-	{
-		aligned_block_size = WORD_SIZE;
-	}
-	else if (0 == remainder) /* block_size is already aligned */
-	{
-		aligned_block_size = block_size;
-	}
-	else 					/* block_size isn't aligned */
-	{
-		aligned_block_size = block_size + WORD_SIZE - remainder;
-	}
-	return aligned_block_size;
-}
+	size_t free_block_offset;
+};
 
 /*
  * Initialize new fixed size allocator
@@ -56,8 +36,9 @@ static size_t AlignBlockSize(size_t block_size)
  */
 fsa_t *FSAInit(void *memory, size_t memory_size, size_t block_size)
 {
-	(fsa_t *) fsa = NULL;
-	size_t block_offset = 1;
+	fsa_t *fsa_pool = NULL;
+	meta_t *metadata = NULL;
+	size_t block_offset = block_size;
 	
 	assert(NULL != memory);
 	assert(0 != block_size);
@@ -69,22 +50,64 @@ fsa_t *FSAInit(void *memory, size_t memory_size, size_t block_size)
 	
 	block_size = AlignBlockSize(block_size);
 	
-	fsa = (fsa_t *) memory;
+	fsa_pool = (fsa_t *) memory;
 	
-	fsa->free_block_index = 1;
+	fsa_pool->free_block_offset = FSA_START_OFFSET;
 
-	meta_t *metadata = (meta_t *)(fsa + (char *)(sizeof(fsa_t)));
+	metadata = (meta_t *)((char *)fsa_pool + FSA_START_OFFSET);
 	
-	for ( block_offset = 0
-		; block_offset < block_size * memory_size
-		; ++block_offset
+	for ( block_offset = block_size
+		; block_offset < memory_size - block_size /* check extreme values for all alignments TODO */
+		; block_offset += block_size, metadata = (meta_t *)((char *) metadata + block_size)
 		)
 	{
-		*metadata = (block_offset * block_size);
+		metadata->block = (block_offset * block_size);
 	}
-	*metadata = 0; /* when all blocks are filled */
+	metadata->block = 0; /* when all blocks are filled pool reaches 0 blocks */
 	
-	return fsa;
+	return fsa_pool;
+}
+
+/* 
+ * helper for init
+ * calculate and return aligned block size 
+ */
+static size_t AlignBlockSize(size_t block_size)
+{
+	size_t remainder = block_size % WORD_SIZE;
+	size_t aligned_block_size = 0;
+	
+	if (block_size < WORD_SIZE)
+	{
+		aligned_block_size = WORD_SIZE;
+	}
+	else if (0 == remainder) /* block_size is already aligned */
+	{
+		aligned_block_size = block_size;
+	}
+	else 					/* block_size isn't aligned */
+	{
+		aligned_block_size = block_size + WORD_SIZE - remainder;
+	}
+	
+	return aligned_block_size;
+}
+
+/* 
+ * helper for free and alloc 
+ * swaps the info for where the next free block is
+ */
+static void SwapBlockInfo(fsa_t *fsa_pool, void *allocated_return_address)
+{
+	size_t temp = 0;
+
+	assert(NULL != fsa_pool);
+	
+	temp = fsa_pool->free_block_offset;
+	
+	fsa_pool->free_block_offset = *(size_t *) allocated_return_address;
+
+	allocated_return_address = &temp;
 }
 
 /*
@@ -93,4 +116,42 @@ fsa_t *FSAInit(void *memory, size_t memory_size, size_t block_size)
  * Return: pointer to allocated memory
  * Errors: if fsa is full, return NULL
  */
-void *FSAAlloc(fsa_t *fsa_pool);
+void *FSAAlloc(fsa_t *fsa_pool)
+{
+	void *allocated_return_address = NULL;
+
+	assert(NULL != fsa_pool);
+	
+	allocated_return_address = (char *)(fsa_pool) + FSA_START_OFFSET + fsa_pool->free_block_offset;
+	
+	if (0 == *(size_t *)allocated_return_address) /* fsa is full */
+	{
+		return NULL; 
+	}
+	
+	SwapBlockInfo(fsa_pool, allocated_return_address);
+	
+	return allocated_return_address;
+}
+
+/*
+ * Count number of free blocks in fixed size allocator
+ * @fsa_pool  : fixed sized allocator
+ * Return: number of free blocks in fsa_pool
+ * Errors: none
+ */
+size_t FSACountFree(const fsa_t *fsa_pool)
+{
+	size_t num_free_blocks = 0;
+	assert(NULL != fsa_pool);
+	
+	
+	
+	return num_free_blocks;
+}
+
+size_t FSASuggestSize(size_t num_of_blocks, size_t block_size)
+{
+	size_t total_block_size = sizeof(meta_t) + AlignBlockSize(block_size);
+	return (sizeof(fsa_t) + num_of_blocks * total_block_size);
+}
--- system_programming/fsa/fsa_test.c
+++ system_programming/fsa/fsa_test.c
@@ -1,13 +1,78 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h> /* malloc free */
 
 #include "fsa.h"
 #include "/home/rd61/ran-shieber/scripts/test_template.c"
 
-/* Forward Devlarations */
+/* Forward Declarations */
+void FSAInit_LessThanWord_test();
+void FSAInit_WordAligned_test();
+void FSAInit_MoreThanWord_test();
+void FSAAlloc_test();
 
 int main()
 {
 	PREVENT_WARNINGS_FROM_UNUSED_FUNCS_FROM_TESTS_TEMPLATE
+	
+/*	FSAInit_LessThanWord_test();*/
+	
+/*	FSAInit_WordAligned_test();*/
+	
+	FSAInit_MoreThanWord_test();
+	
+/*	FSAAlloc_test();*/
+	
 	return 0;
+}
+
+void FSAInit_LessThanWord_test()
+{
+	fsa_t *fsa_pool = NULL;
+	size_t memory_size = 40;
+	size_t block_size = 4;
+	int *memory = (int *)malloc(memory_size);
+	
+	fsa_pool = FSAInit(memory, memory_size,block_size);
+	
+	free(memory);
+}
+
+void FSAInit_WordAligned_test()
+{
+	fsa_t *fsa_pool = NULL;
+	size_t memory_size = 40;
+	size_t block_size = 8;
+	int *memory = (int *)malloc(memory_size);
+	
+	fsa_pool = FSAInit(memory, memory_size,block_size);
+	
+	free(memory);
+}
+
+void FSAInit_MoreThanWord_test()
+{
+	fsa_t *fsa_pool = NULL;
+	size_t memory_size = 40;
+	size_t block_size = 10;
+	int *memory = (int *)malloc(memory_size);
+	
+	fsa_pool = FSAInit(memory, memory_size,block_size);
+	
+	free(memory);
+}
+
+void FSAAlloc_test()
+{
+	fsa_t *fsa_pool = NULL;
+	void *alloc_result = NULL;
+	size_t memory_size = 40;
+	size_t block_size = 4;
+	int *memory = (int *)malloc(memory_size);
+	
+	fsa_pool = FSAInit(memory, memory_size,block_size);
+	
+	alloc_result = FSAAlloc(fsa_pool);
+	
+	free(memory);
 }
