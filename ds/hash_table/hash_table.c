161821dd8297c79f81b277f7421f6ea4192168d1
ran.shieber <ran.shieber1@gmail.com>
2019-10-01 22:41:33 +0300

    hashtable: finished insert, size, isempty

--- ds/hash_table/hash_table.c
+++ ds/hash_table/hash_table.c
@@ -4,6 +4,160 @@
  *   Reviewer : 		    *
  *	 Status   : Sent	    *
  ****************************/
+#include <assert.h>
+#include <stdlib.h> /* malloc free */
 
 #include "hash_table.h"
+#include "dlist.h"
 
+struct hash_table
+{
+	size_t (*hash_func)(void *key);
+	int (*cmp_func)(const void *key1, const void *key2);
+	size_t num_buckets;
+	dlist_t **buckets;
+};
+
+static void InitDlists(dlist_t **buckets, size_t num_of_buckets);
+
+/* Create hash table */
+hash_table_t *HashTableCreate(size_t num_of_buckets,
+							  size_t (*hash_func)(void *data),
+							  int (*hash_cmp_func)(const void *key1,
+							  				  	   const void *key2))
+{
+	hash_table_t *ht = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (NULL == ht)
+	{
+		return NULL;
+	}
+	
+	assert(num_of_buckets > 0);
+	assert(NULL != hash_func);
+	assert(NULL != hash_cmp_func);
+	
+	ht->buckets = (dlist_t **)malloc(num_of_buckets * sizeof(dlist_t*));
+	if (NULL == ht->buckets)
+	{
+		free(ht); ht = NULL;
+		return NULL;
+	}
+	
+	InitDlists(ht->buckets, num_of_buckets);
+	ht->hash_func = hash_func;
+	ht->cmp_func = hash_cmp_func;
+	ht->num_buckets = num_of_buckets;	
+	
+	return ht;
+}
+
+static void InitDlists(dlist_t **buckets, size_t num_of_buckets)
+{
+	int alloc_success = 1;
+	size_t bucket = 0;
+	
+	for (bucket = 0; alloc_success && bucket < num_of_buckets; ++bucket)
+	{
+		buckets[bucket] = DListCreate();
+		if (NULL == buckets[bucket])
+		{
+			alloc_success = 0;
+		}
+	}
+}
+
+/* Destroy the hash table */
+void HashTableDestroy(hash_table_t *hash_table)
+{
+	size_t bucket = 0;
+	
+	assert(NULL != hash_table);
+	
+	for (bucket = 0; bucket < hash_table->num_buckets; ++bucket)
+	{
+		if (NULL == hash_table->buckets)
+		{
+			/* TODO free previously alloced dlists */
+			break;
+		}
+		
+		if (NULL != hash_table->buckets[bucket])
+		{
+			DListDestroy(hash_table->buckets[bucket]);
+		}
+	}
+	free(hash_table->buckets); hash_table->buckets = NULL;
+	free(hash_table); hash_table = NULL;
+}
+
+/* Insret data to the hash table  */
+int HashTableInsert(hash_table_t *hash_table, void *data)
+{
+	size_t bucket_idx = 0;
+	dlist_iter_t end = NULL;
+	dlist_iter_t insert_result = NULL;
+
+	assert(NULL != hash_table);
+	
+	bucket_idx = hash_table->hash_func(data);
+	
+	end = DListEnd((hash_table->buckets[bucket_idx]));
+	insert_result = DListPushFront(hash_table->buckets[bucket_idx], data);
+	
+	if (DListIsSameIter(insert_result, end))
+	{
+		return 1;
+	}
+	
+	return 0;	
+}
+
+/* Remove data from the hash table */
+void HashTableRemove(hash_table_t *hash_table, void *data)
+{
+/*	size_t bucket_idx = 0;*/
+/*	dlist_iter_t found_data = NULL;*/
+/*	dlist_iter_t begin = NULL;*/
+/*	dlist_iter_t end = NULL;	*/
+/*	*/
+/*	assert(NULL != hash_table);*/
+/*	*/
+/*	bucket_idx = hash_table->hash_func(data);*/
+/*	begin = DListBegin((hash_table->buckets[bucket_idx]));*/
+/*	end = DListEnd((hash_table->buckets[bucket_idx]));*/
+/*	*/
+/*	found_data = DListFind(begin, end, hash_table->cmp_func, data);*/
+/*	DListRemove(found_data);*/
+}
+
+/* Get size of the hash table */
+size_t HashTableSize(const hash_table_t *hash_table)
+{
+	size_t size = 0;
+	size_t bucket = 0;	
+	
+	for (bucket = 0; bucket < hash_table->num_buckets; ++bucket)
+	{
+		size += DListSize(hash_table->buckets[bucket]);
+	}
+	
+	return size;
+}
+
+
+/* Check if hash table is empty */
+int HashTableIsEmpty(const hash_table_t *hash_table)
+{
+	size_t bucket = 0;	
+	
+	for (bucket = 0; bucket < hash_table->num_buckets; ++bucket)
+	{
+		int is_empty = DListIsEmpty(hash_table->buckets[bucket]);
+		if (0 == is_empty)
+		{
+			return is_empty;
+		}
+	}
+	
+	return 1;
+}
--- ds/hash_table/hash_table_test.c
+++ ds/hash_table/hash_table_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h> /* strcmp */
 
 #include "hash_table.h"
 
@@ -18,6 +19,8 @@
 	expect_NULL(NULL,"hi");\
 	expect_Not_NULL(&a, "hi");\
 }
+
+#define LEGAL_ALPHA_BUCKETS 172
 
 /*************************************************************************
 								 										 *
@@ -62,13 +65,100 @@ void expect_Not_NULL(void *pointer, char *func_name)
 
 /*************************************************************************
 								 										 *
+				      TEST HELPER FUNCTIONS								 *
+																		 *
+*************************************************************************/
+size_t FirstLetterHashFunc(void *key)
+{
+	char *str_key = NULL;
+	
+	assert(NULL != key);
+	
+	str_key = (char *)key;
+	
+	return (str_key[0] - 65) % LEGAL_ALPHA_BUCKETS;
+}
+int StrCmpFunc(const void *key1, const void *key2)
+{	
+	assert(NULL != key1);
+	assert(NULL != key2);
+	
+	return strcmp((char *)key1, (char *)key2);
+}
+
+void FuncsTest()
+{
+	char *str1 = "A";
+	char *str2 = "z";
+	
+	printf("%ld\n", FirstLetterHashFunc(str1));
+	printf("%ld\n", FirstLetterHashFunc(str2));
+	printf("%d\n", StrCmpFunc(str1, str2));		
+}
+
+/*************************************************************************
+								 										 *
 				      FORWARD DECLARATIONS								 *
 																		 *
 *************************************************************************/
 
+void HashTableCreate_test();
+void HashTableInsert_test();
+/*void HashTableRemove_test();*/
+void HashTableSize_test();
+
+
+
 int main()
 {
-	PREVENT_WARNINGS_FROM_UNUSED_FUNCS_FROM_TESTS_TEMPLATE
+	PREVENT_WARNINGS_FROM_UNUSED_FUNCS_FROM_TESTS_TEMPLATE	  	   	
 	
+/*	FuncsTest();*/
+
+	HashTableCreate_test();
+	
+	HashTableInsert_test();	
+	
+	HashTableSize_test();
+
 	return 0;
+}
+
+void HashTableSize_test()
+{
+	hash_table_t *ht = HashTableCreate (LEGAL_ALPHA_BUCKETS, FirstLetterHashFunc, StrCmpFunc);
+
+	expect_int(HashTableIsEmpty(ht), 1, "HashTableSize_test1");	
+
+	HashTableInsert(ht, "hello");
+	expect_size_t(HashTableSize(ht), 1, "HashTableSize_test3");
+
+	HashTableInsert(ht, "lello");
+	HashTableInsert(ht, "gello");
+	HashTableInsert(ht, "aello");
+	HashTableInsert(ht, "bello");		
+	HashTableInsert(ht, "dello");
+	HashTableInsert(ht, "ddllo");			
+	
+	expect_size_t(HashTableSize(ht), 7, "HashTableSize_test3");	
+	
+	expect_int(HashTableIsEmpty(ht), 0, "HashTableSize_test2");		
+	
+	
+	HashTableDestroy(ht);	
+}
+
+void HashTableInsert_test()
+{
+	hash_table_t *ht = HashTableCreate (LEGAL_ALPHA_BUCKETS, FirstLetterHashFunc, StrCmpFunc);
+	
+	HashTableInsert(ht, "hello");
+	
+	HashTableDestroy(ht);
+}
+
+void HashTableCreate_test()
+{
+	hash_table_t *ht = HashTableCreate (LEGAL_ALPHA_BUCKETS, FirstLetterHashFunc, StrCmpFunc);
+	HashTableDestroy(ht);
 }
