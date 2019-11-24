29ad7d268b8f87842324f97dfe96b6347b99a876
ran.shieber <ran.shieber@gmail.com>
2019-11-24 16:22:33 +0200

    fixing after code review

--- fs/projects/src/il/co/ilrd/vending_machine/VendingMachine.java
+++ fs/projects/src/il/co/ilrd/vending_machine/VendingMachine.java
@@ -2,262 +2,274 @@ package il.co.ilrd.vending_machine;
 
 import java.time.LocalTime;
 import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
 
+/**
+ * 
+ * @author Shieber
+ * @reviewer Daniel
+ * @version 1.3
+ * @since 1.0
+ *
+ */
 public class VendingMachine {
 
-    private State state;
-    private int currBalance;
-    private Collection<Product> products;
-    private ViewModule view;
-    private Thread thread;
-    private boolean active;
-    private LocalTime startTime;
+	private State state = State.INIT;
+	private double currBalance;
+	private List<Product> products;
+	private Consumer<String> view;
+	private Thread thread;
+	private boolean active = true;
+	private LocalTime startTime;
+	private static final int THREAD_INTERVAL_MS = 1000;
 
-    public VendingMachine(Collection<Product> items, ViewModule view) {
-        this.products = items;
-        this.view = view;
-        state = State.INIT;
-        active = true;
-        
-        thread = new Thread ( () -> {
+	public VendingMachine(List<Product> items, Consumer<String> view) {
+		this.products = items;
+		this.view = view;
+	}
 
-            while (active) {
-                state.timeout(this);
-                try {
-					Thread.sleep(1000);
+	/**
+	 * Initializes and invokes VendingMachine to begin state.
+	 */
+	public void start() {
+
+		thread = new Thread(() -> {
+
+			while (active) {
+				state.timeout(this);
+				try {
+					Thread.sleep(THREAD_INTERVAL_MS);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-            }
-        });
-    }
+			}
+		});
 
-    /**
-     * Initializes and invokes VendingMachine to begin state.
-     */
-    public void start() {
-        thread.start();
-        
-        state.init(this);
-    }
+		thread.start();
 
-    /**
-     * Shutdown the VendingMachine.
-     */
-    public void shutDown() {
-        state.shutDown(this);
-    }
+		state.init(this);
+	}
 
-    /**
-     * Insert money into machine.
-     * @param coinValue inserted for transaction.
-     */
-    public void insertCoin(int coinValue) {
-        state.insertCoin(this, coinValue);
-    }
+	/**
+	 * Shutdown the VendingMachine.
+	 */
+	public void shutDown() {
+		state.shutDown(this);
+	}
 
-    /**
-     * Chooses product to buy.
-     * @param chosen product.
-     * invalid product will not have effect,
-     * error message will be displayed in ViewModel.
-     */
-    public void chooseProduct(Product chosen) {
-    	state.chooseProduct(this, chosen);
-    }
+	/**
+	 * Insert money into machine.
+	 * 
+	 * @param coinValue inserted for transaction.
+	 */
+	public void insertCoin(double coinValue) {
+		state.insertCoin(this, coinValue);
+	}
 
-    /**
-     * cancels current transaction, and flushes change
-     */
-    public void cancel() {
-        state.cancel(this);
-    }
+	/**
+	 * Chooses product to buy.
+	 * 
+	 * @param productIndex product. invalid product will not have effect, error message
+	 *               will be displayed in ViewModel.
+	 */
+	public void chooseProduct(int productIndex) {
+		state.chooseProduct(this, productIndex);
+	}
 
-    private enum State {
-        INIT {
-            @Override
-            void init(VendingMachine vm) {
-            	handleCurrentState(vm, IDLE);
-            }
-            
-            @Override
-            void handleCurrentState(VendingMachine vm, State state) {
-            	vm.state = state;
-            }
-        },
+	/**
+	 * cancels current transaction, and flushes change
+	 */
+	public void cancel() {
+		state.cancel(this);
+	}
 
-        IDLE {
-        	@Override
-        	void insertCoin(VendingMachine vm, int coinValue) {
-        		
-        		COLLECTING_MONEY.insertCoin(vm, coinValue);
-        	}
+	private enum State {
+		INIT {
+			@Override
+			void init(VendingMachine vm) {
+				setState(vm, IDLE);
+			}
 
-        	@Override
-        	void handleCurrentState(VendingMachine vm, State state) {
-        		vm.startTime = LocalTime.now();
-        		vm.state = state;
-        	}
-        	
-        },
+			@Override
+			void setState(VendingMachine vm, State state) {
+				vm.state = state;
+			}
+		},
 
-        COLLECTING_MONEY {
-            
-        	@Override
-            void insertCoin(VendingMachine vm, int coinValue) {
-        		vm.currBalance += coinValue;
-        		
-        		handleCurrentState(vm, COLLECTING_MONEY);
-            }
+		IDLE {
+			@Override
+			void insertCoin(VendingMachine vm, double coinValue) {
 
-            @Override
-            void chooseProduct(VendingMachine vm, Product product) {
-        		vm.startTime = LocalTime.now();
-            	
-        		if (!vm.products.contains(product)) {
-            		vm.view.output("Product not available");
-            		
-            		handleCurrentState(vm, COLLECTING_MONEY);
-            		return;
-            	}
-            	
-        		else if (vm.currBalance < product.price) {
-            		vm.view.output("insufficiant funds. current balance is "
-            												  + vm.currBalance);
-            		handleCurrentState(vm, COLLECTING_MONEY);
-            		return;
-            		
-        		}
-            	else {
-            		vm.currBalance -= product.price;
-            		vm.view.output("Dispensing product " + product.name);
-            		handleCurrentState(vm, TRANSACTION_COMPLETE);
-            	}        		
-            }
+				COLLECTING_MONEY.insertCoin(vm, coinValue);
+			}
 
-            void cancel(VendingMachine vm) {
-            	
-            	vm.state.returnChange(vm);
-            	
-            	handleCurrentState(vm, IDLE);
-            	
-            }
+			@Override
+			void setState(VendingMachine vm, State state) {
+				vm.startTime = LocalTime.now();
+				vm.state = state;
+			}
 
-            @Override
-            void timeout(VendingMachine vm) {
-            	if (vm.startTime.plusSeconds(CM_TO).isBefore(LocalTime.now())) {
-            		vm.state.cancel(vm);
-            	}
-            }
-            
-            @Override
-            void handleCurrentState(VendingMachine vm, State state) {
-        		vm.startTime = LocalTime.now();
-            	vm.state = state;
-            }
-            
-        },
+		},
 
-        TRANSACTION_COMPLETE {
-            @Override
-            void timeout(VendingMachine vm) {
-            	            	
-            	if (vm.startTime.plusSeconds(TC_TO).isBefore(LocalTime.now())) {
-            	
-            		vm.view.output("Transaction complete");
-            		
-            		vm.state.returnChange(vm);
-            		
-            		handleCurrentState(vm, IDLE);
-            	}
-            }
-            
-            @Override
-            void handleCurrentState(VendingMachine vm, State state) {
-        		vm.startTime = LocalTime.now();
-            	vm.state = state;
-            }
-        };
+		COLLECTING_MONEY {
 
-        /*
-         *  Will initialize Vending machine, change to waitForProduct state
-         * @param vm - current Vending Machine
-         */
-        void init(VendingMachine vm) {
+			@Override
+			void insertCoin(VendingMachine vm, double coinValue) {
+				vm.currBalance += coinValue;
 
-        }
-        
-        /*
-         *  Will shut down Vending machine, change change active flag to false
-         * @param vm - current Vending Machine
-         */
-        void shutDown(VendingMachine vm) {
-        	vm.active = false;
-        	
-        	try {
+				setState(vm, COLLECTING_MONEY);
+			}
+
+			@Override
+			void chooseProduct(VendingMachine vm, int productIndex) {
+				vm.startTime = LocalTime.now();
+
+				if (productIndex < 0 &&
+						vm.products.size() - 1 < productIndex) {
+					vm.view.accept("Product not available");
+
+					setState(vm, COLLECTING_MONEY);
+					return;
+				}
+
+		else if (vm.currBalance < vm.products.indexOf(productIndex).price) {
+					vm.view.accept("Insufficient funds. current balance is "
+															 + vm.currBalance);
+					setState(vm, COLLECTING_MONEY);
+					return;
+
+				} else {
+					vm.currBalance -= vm.products.indexOf(productIndex).price;
+					vm.view.accept("Dispensing product " +
+										vm.products.indexOf(productIndex).name);
+					setState(vm, TRANSACTION_COMPLETE);
+				}
+			}
+
+			void cancel(VendingMachine vm) {
+
+				vm.state.returnChange(vm);
+
+				setState(vm, IDLE);
+
+			}
+
+			@Override
+			void timeout(VendingMachine vm) {
+				if (vm.startTime.plusSeconds(CM_TO).isBefore(LocalTime.now())) {
+					vm.state.cancel(vm);
+				}
+			}
+
+			@Override
+			void setState(VendingMachine vm, State state) {
+				vm.startTime = LocalTime.now();
+				vm.state = state;
+			}
+
+		},
+
+		TRANSACTION_COMPLETE {
+			@Override
+			void timeout(VendingMachine vm) {
+
+				if (vm.startTime.plusSeconds(TC_TO).isBefore(LocalTime.now())) {
+
+					vm.view.accept("Transaction complete");
+
+					vm.state.returnChange(vm);
+
+					setState(vm, IDLE);
+				}
+			}
+
+			@Override
+			void setState(VendingMachine vm, State state) {
+				vm.startTime = LocalTime.now();
+				vm.state = state;
+			}
+		};
+
+		/*
+		 * Will initialize Vending machine, change to waitForProduct state
+		 * 
+		 * @param vm - current Vending Machine
+		 */
+		void init(VendingMachine vm) {
+
+		}
+
+		/*
+		 * Will shut down Vending machine, change change active flag to false
+		 * 
+		 * @param vm - current Vending Machine
+		 */
+		void shutDown(VendingMachine vm) {
+			vm.active = false;
+
+			try {
 				vm.thread.join();
 			} catch (InterruptedException e) {
 				System.err.println("error joining thread");
 			}
-        }
-        /*
-         * Handles payment transaction, increases current balance,
-         * changes state to WAIT_FOR_PRODUCT if there is sufficient balance.
-         */
-        void insertCoin(VendingMachine vm, int money) {
+		}
 
-        }
+		/*
+		 * Handles payment transaction, increases current balance, changes state to
+		 * WAIT_FOR_PRODUCT if there is sufficient balance.
+		 */
+		void insertCoin(VendingMachine vm, double coin) {
 
-        /*
-         * Changes selected product in vm, changes state to waitForMoney
-         */
-        void chooseProduct(VendingMachine vm, Product product) {
-        }
+		}
 
-        /*
-         * @param vm cancels current transaction,
-         *  changes state to waitForProduct
-         */
-        void cancel(VendingMachine vm) {
-        }
+		/*
+		 * Changes selected product in vm, changes state to waitForMoney
+		 */
+		void chooseProduct(VendingMachine vm, int productIndex) {
+		}
 
-        /*
-         * @param vm 
-         *  handles timeout event 
-         */
-        void timeout(VendingMachine vm) {
-        }
-        
-        /*
-         * @param vm 
-         * sets start time to current time of state transition
-         */
-        abstract void handleCurrentState(VendingMachine vm, State state);
-        
-        void returnChange(VendingMachine vm) {
-        	vm.view.output("flushing " + vm.currBalance + " in change.");
-        	vm.currBalance = 0;
-        }
-        
-        /* Timeouts for Trancaction Complete and Collecting Money states */
-        private final static int TC_TO = 5; 
-        private final static int CM_TO = 10;
-    }
+		/*
+		 * @param vm cancels current transaction, changes state to waitForProduct
+		 */
+		void cancel(VendingMachine vm) {
+		}
 
-    public static class Product {
-        private String name;
-        private int price;
+		/*
+		 * @param vm handles timeout event
+		 */
+		void timeout(VendingMachine vm) {
+		}
 
-        Product(String name, int price) {
-            this.price = price;
-            this.name = name;
-        }
+		/*
+		 * @param vm sets start time to current time of state transition
+		 */
+		abstract void setState(VendingMachine vm, State state);
+
+		void returnChange(VendingMachine vm) {
+			vm.view.accept("flushing " + vm.currBalance + " in change.");
+			vm.currBalance = 0;
+		}
+
+		/* Timeouts for Trancaction Complete and Collecting Money states */
+		private final static int TC_TO = 5;
+		private final static int CM_TO = 10;
+	}
+
+	public static class Product {
+		private String name;
+		private int price;
+		final static private int PRIME = 31;
+
+		Product(String name, int price) {
+			this.price = price;
+			this.name = name;
+		}
 
 		@Override
 		public int hashCode() {
-			final int prime = 31;
 			int result = 1;
-			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			result = PRIME * result + ((name == null) ? 0 : name.hashCode());
 			return result;
 		}
 
@@ -278,6 +290,6 @@ public class VendingMachine {
 			return true;
 		}
 
-    }
+	}
 
 }
