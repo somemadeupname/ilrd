e5da364bbc537c3a227abaab2541817d04eac3bc
ran.shieber <ran.shieber@gmail.com>
2019-11-24 13:11:08 +0200

    sent to review

--- fs/projects/src/il/co/ilrd/vending_machine/VendingMachine.java
+++ fs/projects/src/il/co/ilrd/vending_machine/VendingMachine.java
@@ -1,41 +1,59 @@
 package il.co.ilrd.vending_machine;
 
+import java.time.LocalTime;
 import java.util.Collection;
 
 public class VendingMachine {
 
     private State state;
     private int currBalance;
-    private Product selectedProduct;
-    private Collection<Product> items;
-    private ViewModel view;
+    private Collection<Product> products;
+    private ViewModule view;
+    private Thread thread;
+    private boolean active;
+    private LocalTime startTime;
 
-    public VendingMachine(Collection<Product> items, ViewModel view) {
-    	this.items = items;
+    public VendingMachine(Collection<Product> items, ViewModule view) {
+        this.products = items;
         this.view = view;
-        state = State.begin;
+        state = State.INIT;
+        active = true;
+        
+        thread = new Thread ( () -> {
+
+            while (active) {
+                state.timeout(this);
+                try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+            }
+        });
     }
 
     /**
      * Initializes and invokes VendingMachine to begin state.
      */
     public void start() {
-    	state.init(this);
+        thread.start();
+        
+        state.init(this);
+    }
+
+    /**
+     * Shutdown the VendingMachine.
+     */
+    public void shutDown() {
+        state.shutDown(this);
     }
 
     /**
      * Insert money into machine.
-     * @param money inserted for transaction.
+     * @param coinValue inserted for transaction.
      */
-    public void pay(int money) {
-    	
-    	currBalance += money;
-    	
-    	if (currBalance >= selectedProduct.price) {
-    		
-    		state = State.waitForProduct;/* TODO should add more state? */
-    	}
-
+    public void insertCoin(int coinValue) {
+        state.insertCoin(this, coinValue);
     }
 
     /**
@@ -45,49 +63,120 @@ public class VendingMachine {
      * error message will be displayed in ViewModel.
      */
     public void chooseProduct(Product chosen) {
+    	state.chooseProduct(this, chosen);
     }
 
     /**
      * cancels current transaction, and flushes change
      */
-    public void cancel() {}
+    public void cancel() {
+        state.cancel(this);
+    }
 
     private enum State {
-        begin{
-                @Override
-                void init(VendingMachine vm) {
-                	vm.state = waitForProduct;
-                }
-                @Override
-                void cancel(VendingMachine vm){}
-				@Override
-				void timeout(VendingMachine vm) {
-					// TODO Auto-generated method stub
-				}
+        INIT {
+            @Override
+            void init(VendingMachine vm) {
+            	vm.state.handleCurrentState(vm, IDLE);
+            }
+            
+            @Override
+            void handleCurrentState(VendingMachine vm, State state) {
+            	vm.state = state;
+            }
         },
 
-        waitForMoney{
-            @Override
-            void pay(VendingMachine vm, int money){}
-            @Override
-            void cancel (VendingMachine vm){}
-			@Override
-			void timeout(VendingMachine vm) {
-				// TODO Auto-generated method stub
-				
-			}
+        IDLE {
+        	@Override
+        	void insertCoin(VendingMachine vm, int coinValue) {
+        		
+        		COLLECTING_MONEY.insertCoin(vm, coinValue);
+        	}
+
+        	@Override
+        	void handleCurrentState(VendingMachine vm, State state) {
+        		vm.startTime = LocalTime.now();
+        		vm.state = state;
+        	}
+        	
         },
 
-        waitForProduct{
+        COLLECTING_MONEY {
+            
+        	@Override
+            void insertCoin(VendingMachine vm, int coinValue) {
+        		vm.currBalance += coinValue;
+        		
+        		vm.state.handleCurrentState(vm, COLLECTING_MONEY);
+            }
+
             @Override
-            void chooseProduct(VendingMachine vm, Product product){}
+            void chooseProduct(VendingMachine vm, Product product) {
+        		vm.startTime = LocalTime.now();
+            	
+        		if (!vm.products.contains(product)) {
+            		vm.view.output("Product not available");
+            		
+            		vm.state.handleCurrentState(vm, COLLECTING_MONEY);
+            	}
+            	
+        		else if (vm.currBalance < product.price) {
+            		vm.view.output("insufficiant funds. current balance is " + vm.currBalance);
+            		
+            		vm.state.handleCurrentState(vm, COLLECTING_MONEY);
+        		}
+            	else {
+            		vm.currBalance -= product.price;
+            		vm.view.output("Dispensing product " + product.name);
+            	}
+        		
+            	
+            	vm.state.handleCurrentState(vm, TRANSACTION_COMPLETE);
+            	
+            }
+
+            void cancel(VendingMachine vm) {
+            	
+            	vm.state.returnChange(vm);
+            	
+            	vm.state.handleCurrentState(vm, IDLE);
+            	
+            }
+
             @Override
-            void cancel (VendingMachine vm){}
-			@Override
-			void timeout(VendingMachine vm) {
-				// TODO Auto-generated method stub
-				
-			}
+            void timeout(VendingMachine vm) {
+            	if (vm.startTime.plusSeconds(CM_TO).isBefore(LocalTime.now())) {
+            		vm.state.cancel(vm);
+            	}
+            }
+            
+            @Override
+            void handleCurrentState(VendingMachine vm, State state) {
+        		vm.startTime = LocalTime.now();
+            	vm.state = state;
+            }
+            
+        },
+
+        TRANSACTION_COMPLETE {
+            @Override
+            void timeout(VendingMachine vm) {
+            	            	
+            	if (vm.startTime.plusSeconds(TC_TO).isBefore(LocalTime.now())) {
+            	
+            		vm.view.output("Transaction complete");
+            		
+            		vm.state.returnChange(vm);
+            		
+            		vm.state.handleCurrentState(vm, IDLE);
+            	}
+            }
+            
+            @Override
+            void handleCurrentState(VendingMachine vm, State state) {
+        		vm.startTime = LocalTime.now();
+            	vm.state = state;
+            }
         };
 
         /*
@@ -95,56 +184,100 @@ public class VendingMachine {
          * @param vm - current Vending Machine
          */
         void init(VendingMachine vm) {
-        	vm.state = waitForProduct;
+
         }
-       /*
-        * Handles payment transaction, increases current balance,
-        * changes state to chooseProduct if there is sufficient balance.
-        */
-        void pay(VendingMachine vm, int money) {
+        
+        /*
+         *  Will shut down Vending machine, change change active flag to false
+         * @param vm - current Vending Machine
+         */
+        void shutDown(VendingMachine vm) {
+        	vm.active = false;
         	
+        	try {
+				vm.thread.join();
+			} catch (InterruptedException e) {
+				System.err.println("error joining thread");
+			}
+        }
+        /*
+         * Handles payment transaction, increases current balance,
+         * changes state to WAIT_FOR_PRODUCT if there is sufficient balance.
+         */
+        void insertCoin(VendingMachine vm, int money) {
+
         }
 
-       /*
-        * Changes selected product in vm, changes state to waitForMoney
-        */
+        /*
+         * Changes selected product in vm, changes state to waitForMoney
+         */
         void chooseProduct(VendingMachine vm, Product product) {
-        	
         }
 
         /*
          * @param vm cancels current transaction,
          *  changes state to waitForProduct
          */
-        void cancel(VendingMachine vm) {}
-        
-        abstract void timeout(VendingMachine vm);
-       
+        void cancel(VendingMachine vm) {
         }
-}
 
-    class Product {
-        public String name;
-        public int price;
+        /*
+         * @param vm 
+         *  handles timeout event 
+         */
+        void timeout(VendingMachine vm) {
+        }
+        
+        /*
+         * @param vm 
+         * sets start time to current time of state transition
+         */
+        abstract void handleCurrentState(VendingMachine vm, State state);
+        
+        void returnChange(VendingMachine vm) {
+        	vm.view.output("flushing " + vm.currBalance + " in change.");
+        	vm.currBalance = 0;
+        }
+        
+        /* Timeouts for Trancaction Complete and Collecting Money states */
+        final static int TC_TO = 5; 
+        final static int CM_TO = 10;
+    }
+
+    public static class Product {
+        private String name;
+        private int price;
 
         Product(String name, int price) {
             this.price = price;
             this.name = name;
         }
-        
-        @Override
-        public int hashCode() {
-        	// TODO Auto-generated method stub
-        	return super.hashCode();
-        }
-        
-        @Override
-        public boolean equals(Object obj) {
-        	
-        	
-        	return super.equals(obj);
-        }
-        
-        
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Product other = (Product) obj;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			return true;
+		}
+
     }
 
+}
--- fs/projects/src/il/co/ilrd/vending_machine/ViewModel.java
+++
@@ -1,9 +0,0 @@
-package il.co.ilrd.vending_machine;
-
-public interface ViewModel {
-    /**
-     *
-     * @param msg to be displayed in view
-     */
-    public void output(String msg);
-}
