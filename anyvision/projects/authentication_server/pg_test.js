4d33ce54fb8840d1fabc79850d268b76caf9e4a9
ran.shieber <ran.shieber1@gmail.com>
2020-02-09 11:32:17 +0200

    switched Client with Pool

--- anyvision/projects/authentication_server/pg_test.js
+++ anyvision/projects/authentication_server/pg_test.js
@@ -1,20 +1,29 @@
-const { Client } = require('pg');
-const client = new Client({
+const { Pool } = require('pg');
+const pool = new Pool({
   host: 'localhost',
   port: 5432,
   user: 'student',
   password: 'ct,h kvmkhj',
-  database: 'computer_firm'
+  database: 'users'
 });
-client.connect();
-const query = client.query('SELECT * FROM laptop')
+pool.connect();
+const username = 'john123';
+const password = 'smith';
+const query = pool.query(`SELECT username, password FROM users WHERE
+                                  username='${username}' AND password='${password}'`)
     .then((result) => {
-      console.log(result.rows);
+      if (result.rowCount === 0) {
+        console.log('Incorrect username or password.');
+        console.log('FAILED.\n');
+        console.log(result);
+      } else {
+        console.log(result.rows);
+      }
     })
     .catch((e) => {
       console.log(e.stack);
     })
-    .then(() => client.end());
+    .then(() => pool.end());
 // client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
 //   console.log(err ? err.stack : res.rows[0].message); // Hello World!
 //   client.end();
